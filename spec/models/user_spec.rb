e594e9a40d709b75500cd04009db6cbb64550124
azi.chen <azi.chen@gmail.com>
2015-03-14 16:10:02 +0800

    Add first step User Model unit Test
    	new file:   spec/models/user_spec.rb

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe User do
+  it "is valid with a nickname, avatar, token, os, os_version"
+  it "is invalid without a nickname"
+  it "is invalid without a avatar"
+  it "is invalid without a device token"
+  it "is invalid without a device os"
+  it "is invalid without a device os version"
+ 	it "is invalid with a duplicate device token"
+end
