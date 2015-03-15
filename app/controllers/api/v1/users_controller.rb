ba3ce53c2452e9d99618821a1cd6f5bd61cce1da
azi.chen <azi.chen@gmail.com>
2015-03-15 20:58:49 +0800

    Add Api::V1::UsersController
    	new file:   app/controllers/api/base_controller.rb
    	new file:   app/controllers/api/v1/users_controller.rb
    	new file:   config/initializers/carrierwave.rb
    	modified:   config/routes.rb

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::UsersController < Api::BaseController
+
+	def add_device
+		@user_device = User.new(user_params)
+		if @user_device.save
+			render json: { success: true }, status: :created
+		else
+			Rails.logger.info "#{@user_device.errors.inspect}"
+			render json: { success: false, message: @user_device.errors }, status: :bad_request
+		end
+	end
+
+	private
+
+	def user_params
+   	params.require(:user).permit(:nickname, :avatar, 
+   		             :device_token, :device_os,:device_os_version)
+ 	end
+
+end
