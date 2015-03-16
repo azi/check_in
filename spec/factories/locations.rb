b6f4ee64d74dd5d55240727490c1be6e7c2e17e3
azi.chen <azi.chen@gmail.com>
2015-03-16 13:36:23 +0800

    Add Location API request spec
    	modified:   spec/factories/locations.rb
    	new file:   spec/requests/api/v1/locations_spec.rb

--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -2,11 +2,38 @@
 
 FactoryGirl.define do
   factory :location do
+  	association :user
     lng "121.56481"
     lat  "25.033718"
     pic { Rack::Test::UploadedFile.new("#{Rails.root}/spec/factories/avatar.jpg") }
     name "101大樓"
     message "打卡訊息假資料"
-    user_id 1
+  end
+
+  factory :location_1 do
+  	association :user
+    lng "121.560296"
+    lat  "25.039907"
+    pic { Rack::Test::UploadedFile.new("#{Rails.root}/spec/factories/avatar.jpg") }
+    name "國父記念館"
+    message "國父記念館－打卡訊息假資料"
+  end
+
+  factory :location_50 do
+  	association :user
+    lng "121.739466"
+    lat  "25.132301"
+    pic { Rack::Test::UploadedFile.new("#{Rails.root}/spec/factories/avatar.jpg") }
+    name "基隆火車站"
+    message "基隆火車站－打卡訊息假資料"
+  end
+
+  factory :location_500 do
+  	association :user
+    lng "120.30194"
+    lat  "22.669167"
+    pic { Rack::Test::UploadedFile.new("#{Rails.root}/spec/factories/avatar.jpg") }
+    name "高雄巨蛋"
+    message "高雄巨蛋－打卡訊息假資料"
   end
 end
---
+++ spec/requests/api/v1/locations_spec.rb
@@ -0,0 +1,100 @@
+require 'spec_helper'
+
+describe "Location API" do
+	describe "Post location" do
+		before :each do
+			@user = attributes_for(:user)
+			@location = attributes_for(:location)
+			@post_params = {"token" => @user[:device_token], "locations" => @location} 
+		end
+		context "with valid attributes" do
+			it "saves the new location in the database" do
+				expect{
+					post api_v1_new_location_path, @post_params
+				}.to change(Contact, :count).by(1)
+				expect(response.status).to eq 201
+			end
+		end
+		context "with invalid attributes" do
+			it "does not save the new location in the database" do
+				@post_params["locations"]["lat"] = nil
+				expect{
+					post api_v1_new_location_path, post_params
+				}.to_not change(Location, :count)
+				expect(response.status).to eq 422
+			end
+		end
+	end
+	
+	describe "Edit location" do	
+		before :each do
+			@user = create(:user)
+			@location = create(:location, user:@user)
+		end
+		context "valid attributes" do
+			it "locates the requested @location" do
+				post_params = {"token" => @user.device_token, "locations" => @location.attributes}
+				patch api_v1_location_path, {id: @location}, post_params
+				expect(assigns(:location)).to eq(@location)
+			end
+			it "changes @location's attributes" do
+				@location.name = "101 商辦大樓"
+				@location.message = "修改打卡點訊息測試"
+				post_params = {"token" => @user.device_token, "locations" => @location.attributes}
+				patch api_v1_location_path, {id: @location}, post_params
+				@location.reload
+				expect(@location.name).to eq("101 商辦大樓")
+				expect(@location.message).to eq(修改打卡點訊息測試)
+			end
+		end
+
+		context "with invalid attributes" do
+	 		it "does not change the location's attributes" do
+				@location.name = nil
+				@location.message = "哇勒"
+				post_params = {"token" => @user.device_token, "locations" => @location.attributes}
+				patch api_v1_location_path, {id: @location}, post_params
+				@location.reload
+				expect(@contact.name).to eq("101 商辦大樓")
+				expect(@location.message).to_not eq("哇勒")
+				expect(response.status).to eq 204
+			end
+		end	
+	end
+
+	describe "Delete location" do
+		before :each do
+			@location = create(:location)
+		end
+		it "deletes the location" do
+			expect{
+				delete :destroy, id: @location
+			}.to change(Location,:count).by(-1)
+			expect(response.status).to eq 204
+		end
+	end
+	describe "Search locations" do
+		before :each do
+			@user = create(:user)
+			@user2 = create(:user)
+			@user3 = create(:user)
+			@location1 = create(:location, user:@user)
+			@location2 = create(:location_50, user:@user)
+			@location3 = create(:location_50, user:@user2)
+			@location4 = create(:location_500, user:@user2)
+			@location5 = create(:location_1, user:@user2)
+			@location6 = create(:location_500, user:@user3)
+		end
+		context "with params[:range]" do
+			it "return json of locations in range" do
+				post_params = {"token" => @user.device_token, 
+					             "current_lat" => @location.lat,
+					             "current_lng" => @location.lng, 
+					             "range" => 50
+					            }
+				get api_v1_search_location, post_params
+				expect(assigns(:locations)).to match_array([@location2,@location3,@location5])
+			end	
+		end
+	end
+end
